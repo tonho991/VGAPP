8488fccff01840006676a66ad4ee527b8ac7eb2f
tonho991 <67771574+tonho991@users.noreply.github.com>
2021-08-28 20:44:27 -0400

    app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ function getDivVideos(keys, json){
      var idV = video.url_v.replace("https://youtu.be/", "");
      divV += `
   <div class="${generateClass()}">
-    <div onclick="window.open('https://vgapp-site.herokuapp.c/video?id=${idV}','_self')" style="background:#dddbe6;padding:8px;margin-style:solid;border-color:#bcbcbc;margin-bottom:20px;border-radius:5px;">
+    <div onclick="window.open('https://vgapp-site.herokuapp.com/video?id=${idV}','_self')" style="background:#dddbe6;padding:8px;margin-style:solid;border-color:#bcbcbc;margin-bottom:20px;border-radius:5px;">
      <img src="https://i.ytimg.com/vi/${idV}/default.jpg"/><br>
      <p class="vidtitle">${video.nome_v}</p>
     </div>
