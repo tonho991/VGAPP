e92cf4c451896c6be9b9b26c0544eea38d531519
Marco Antonio <67771574+tonho991@users.noreply.github.com>
2023-11-15 16:07:43 -0300

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -3,14 +3,7 @@ const firebase = require("firebase");
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCRrypgeFaPgqPUCypc1NzAu4zlaEzzmsU",
-  authDomain: "vgapp-9cc66.firebaseapp.com",
-  databaseURL: "https://vgapp-9cc66-default-rtdb.firebaseio.com",
-  projectId: "vgapp-9cc66",
-  storageBucket: "vgapp-9cc66.appspot.com",
-  messagingSenderId: "119617741588",
-  appId: "1:119617741588:web:9ebc96ccaf249321545801",
-  measurementId: "G-XJ7E3K3LTE"
+ 
 };
   
 if (!firebase.apps.length) {
